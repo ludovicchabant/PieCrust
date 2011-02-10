e8dacc70c79d4555bd5a3a2bae410e388fab28de
ludovic_chabant <none@none>
2011-02-09 16:03:14 -0800

    Added global error handler.

--- website/_piecrust/PieCrustException.class.php
+++ website/_piecrust/PieCrustException.class.php
@@ -3,3 +3,10 @@
 class PieCrustException extends Exception
 {
 }
+
+function piecrust_error_handler($errno, $errstr, $errfile = null, $errline = 0, $errcontext = null)
+{
+	throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
+}
+
+set_error_handler(piecrust_error_handler);
