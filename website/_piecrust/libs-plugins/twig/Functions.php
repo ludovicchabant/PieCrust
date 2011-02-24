0da8df772952d0f7db7f71d57e85ceaeaa7cfd52
Ludovic Chabant <ludovic@chabant.com>
2011-02-23 22:54:34 -0800

    Now implemented a proper Twig extension.

--- website/_piecrust/libs-plugins/twig/Functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-function twig_pcurl_function($value)
-{
-    return TwigTemplateEngine::getPathPrefix() . $value;
-}
---
+++ website/_piecrust/libs-plugins/twig/PieCrustExtension.php
@@ -0,0 +1,52 @@
+<?php
+
+require_once (PIECRUST_APP_DIR . 'Paginator.class.php');
+
+
+class PieCrustExtension extends Twig_Extension
+{
+    protected $pieCrust;
+	protected $pathPrefix;
+    protected $tagUrlFormat;
+    protected $categoryUrlFormat;
+    
+    public function __construct(PieCrust $pieCrust)
+    {
+        $this->pieCrust = $pieCrust;
+        
+        $usePrettyUrls = ($pieCrust->getConfigValue('site','pretty_urls') === true);		
+		$this->pathPrefix = ($pieCrust->getHost() . $pieCrust->getUrlBase() . ($usePrettyUrls ? '' : '?/'));
+        
+        $this->tagUrlFormat = $pieCrust->getConfigValue('site', 'tags_urls');
+        $this->categoryUrlFormat = $pieCrust->getConfigValue('site', 'categories_urls');
+    }
+    
+    public function getName()
+    {
+        return "piecrust";
+    }
+    
+    public function getFunctions()
+    {
+        return array(
+            'pcurl'    => new Twig_Function_Method($this, 'getUrl'),
+            'pctagurl' => new Twig_Function_Method($this, 'getTagUrl'),
+            'pccaturl' => new Twig_Function_Method($this, 'getCategoryUrl')
+        );
+    }
+    
+    public function getUrl($value)
+    {
+        return $this->pathPrefix . $value;
+    }
+    
+    public function getTagUrl($value)
+    {
+        return Paginator::buildTagUrl($this->tagUrlFormat, $value);
+    }
+    
+    public function getCategoryUrl($value)
+    {
+        return Paginator::buildCategoryUrl($this->categoryUrlFormat, $value);
+    }
+}
