f60a0b615233515cdb018e68e687664787759174
ludovic_chabant <none@none>
2011-02-24 14:12:18 -0800

    Changes in PieCrust class. More customization possible on the importer class now.

--- website/_piecrust/PieCrustImporter.class.php
+++ website/_piecrust/PieCrustImporter.class.php
@@ -5,27 +5,63 @@ require_once 'IImporter.class.php';
 
 define('PIECRUST_IMPORT_DIR', '_import');
 
+/**
+ * A class that bootstraps the importer classes to import content into a PieCrust website.
+ */
 class PieCrustImporter
 {
-	public $postsFs;
+	protected $pieCrust;
 	
-	public function __construct()
+	protected $importDir;
+	/**
+	 * Gets the import directory.
+	 */
+	public function getImportDir()
 	{
-		$this->postsFs = 'hierarchy';
+		if ($this->importDir === null)
+		{
+            $this->setImportDir($this->pieCrust()->getRootDir() . PIECRUST_IMPORT_DIR);
+		}
+        return $this->importDir;
 	}
 	
-	public function importContent()
+	/**
+	 * Sets the import directory.
+	 */
+	public function setImportDir($dir)
 	{
-		$files = new FilesystemIterator(PIECRUST_ROOT_DIR . PIECRUST_IMPORT_DIR);
+		$this->importDir = $dir;
+		if (is_dir($this->importDir) === false)
+		{
+            throw new PieCrustException('The import directory doesn\'t exist: ' . $this->importDir);
+		}
+	}
+	
+	/**
+	 * Creates a new instance of PieCrustImporter.
+	 */
+	public function __construct(PieCrust $pieCrust)
+	{
+		$this->pieCrust = $pieCrust;
+	}
+	
+	/**
+	 * Imports content found in the import directory.
+	 */
+	public function import()
+	{
+		$files = new FilesystemIterator($this->getImportDir());
 		foreach ($files as $f)
 		{
 			echo 'Importing: ' . $f->getFilename() . "\n";
+			
 			$fi = pathinfo($f->getFilename());
 			$type = $fi['filename'] . 'Importer';
+			
 			require_once ('importers' . DIRECTORY_SEPARATOR . $type . '.class.php');
 			$importer = new $type();
 			$importer->open($f->getPathname());
-			$importer->importPosts(PIECRUST_ROOT_DIR . PIECRUST_CONTENT_POSTS_DIR, $this->postsFs);
+			$importer->importPosts($this->pieCrust()->getPostsDir(), $this->pieCrust->getConfigValue('site', 'posts_fs'));
 			$importer->close();
 		}
 	}
