5ee2e5ce1db399222f7fd5dfceb0c7ab6eb39464
ludovic_chabant <none@none>
2011-02-23 17:07:12 -0800

    Made the PageRenderer's HTML header setting code available as a utility function.

--- website/_piecrust/PageRenderer.class.php
+++ website/_piecrust/PageRenderer.class.php
@@ -33,32 +33,7 @@ class PageRenderer
 		// Set the HTML header and render the page.
 		if ($outputHeaders === true)
 		{
-			switch ($pageConfig['content_type'])
-			{
-				case 'html':
-				default:
-					header("Content-type: text/html; charset=utf-8");
-					break;
-				case 'xml':
-					header("Content-type: text/xml; charset=utf-8");
-					break;
-				case 'txt':
-				case 'text':
-					header("Content-type: text/plain; charset=utf-8");
-					break;
-				case 'css':
-					header('Content-type: text/css; charset=utf-8');
-					break;
-				case 'atom':
-					header("Content-type: application/atom+xml; charset=utf-8");
-					break;
-				case 'rss':
-					header("Content-type: application/rss+xml; charset=utf-8");
-					break;
-				case 'json':
-					header('Content-type: application/json; charset=utf-8');
-					break;
-			}
+			PageRenderer::setHeaders($pageConfig['content_type']);
 		}
 		
 		echo "<!-- PieCrust " . PieCrust::VERSION . " - " . ($page->isCached() ? "baked this morning!" : "baked just now!") . " -->\n";
@@ -70,6 +45,36 @@ class PageRenderer
 		ob_start();
 		$this->render($page, $extraData, $outputHeaders);
 		return ob_get_clean();
+	}
+	
+	public static function setHeaders($contentType)
+	{
+		switch ($contentType)
+		{
+			case 'html':
+			default:
+				header("Content-type: text/html; charset=utf-8");
+				break;
+			case 'xml':
+				header("Content-type: text/xml; charset=utf-8");
+				break;
+			case 'txt':
+			case 'text':
+				header("Content-type: text/plain; charset=utf-8");
+				break;
+			case 'css':
+				header('Content-type: text/css; charset=utf-8');
+				break;
+			case 'atom':
+				header("Content-type: application/atom+xml; charset=utf-8");
+				break;
+			case 'rss':
+				header("Content-type: application/rss+xml; charset=utf-8");
+				break;
+			case 'json':
+				header('Content-type: application/json; charset=utf-8');
+				break;
+		}
 	}
 }
 
