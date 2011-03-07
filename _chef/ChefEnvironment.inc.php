fc3c3c9d14956bbad680e34ed14b4e7d4400a89b
Ludovic Chabant <ludovic@chabant.com>
2011-03-06 23:15:49 -0800

    Added check for PHP version.

--- _chef/ChefEnvironment.inc.php
+++ _chef/ChefEnvironment.inc.php
@@ -5,3 +5,7 @@ set_include_path(get_include_path() . PATH_SEPARATOR .
 				 (dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '_piecrust') . PATH_SEPARATOR .
 				 (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'libs'));
 
+if (!defined('PHP_VERSION_ID') or PHP_VERSION_ID < 50300)
+{
+    die("You need PHP 5.3+ to use PieCrust.");
+}
