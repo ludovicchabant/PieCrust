8b350a795a793d3ee011d2bd57c2923b90ea3a69
ludovicchabant <none@none>
2011-03-11 14:35:13 -0800

    Add "is_self" key on linkers.

--- _piecrust/Linker.class.php
+++ _piecrust/Linker.class.php
@@ -5,52 +5,68 @@ define('PIECRUST_LINKER_DIR_SUFFIX', '_');
 require_once 'PageWrapper.class.php';
 
 /**
- *
+ * A class that exposes the list of pages in a folder to another page.
  */
 class Linker implements ArrayAccess, Iterator
 {
     protected $pieCrust;
     protected $baseDir;
+    protected $selfKey;
     protected $linksCache;
     
-    public function __construct(PieCrust $pieCrust, $baseDir)
+    /**
+     * Creates a new instance of Linker.
+     */
+    public function __construct(PieCrust $pieCrust, $pageOrBaseDir)
     {
         $this->pieCrust = $pieCrust;
-        $this->baseDir = $baseDir;
+        if ($pageOrBaseDir instanceof Page)
+        {
+            $this->baseDir = dirname($pageOrBaseDir->getPath()) . DIRECTORY_SEPARATOR;
+            $this->selfKey = basename($pageOrBaseDir->getPath(), '.html');
+        }
+        else
+        {
+            $this->baseDir = $pageOrBaseDir;
+            $this->selfKey = null;
+        }
     }
     
+    // {{{ ArrayAccess members
     public function __isset($name)
-	{
-		return $this->offsetExists($name);
-	}
-	
-	public function __get($name)
-	{
-		return $this->offsetGet($name);
-	}
-	
-	public function offsetExists($offset)
-	{
-		$this->ensureLinksCache();
-		return isset($this->linksCache[$offset]);
-	}
-	
-	public function offsetGet($offset) 
-	{
-		$this->ensureLinksCache();
-		return $this->linksCache[$offset];
-	}
-	
-	public function offsetSet($offset, $value)
-	{
-		throw new PieCrustException('Linker is read-only.');
-	}
-	
-	public function offsetUnset($offset)
-	{
-		throw new PieCrustException('Linker is read-only.');
-	}
+    {
+        return $this->offsetExists($name);
+    }
     
+    public function __get($name)
+    {
+        return $this->offsetGet($name);
+    }
+    
+    public function offsetExists($offset)
+    {
+        $this->ensureLinksCache();
+        return isset($this->linksCache[$offset]);
+    }
+    
+    public function offsetGet($offset) 
+    {
+        $this->ensureLinksCache();
+        return $this->linksCache[$offset];
+    }
+    
+    public function offsetSet($offset, $value)
+    {
+        throw new PieCrustException('Linker is read-only.');
+    }
+    
+    public function offsetUnset($offset)
+    {
+        throw new PieCrustException('Linker is read-only.');
+    }
+    // }}}
+    
+    // {{{ Iterator members
     public function rewind()
     {
         $this->ensureLinksCache();
@@ -85,6 +101,7 @@ class Linker implements ArrayAccess, Iterator
         $this->ensureLinksCache();
         return key($this->linksCache) !== null;
     }
+    // }}}
     
     protected function ensureLinksCache()
     {
@@ -98,7 +115,8 @@ class Linker implements ArrayAccess, Iterator
                 if ($item->isDir())
                 {
                     $key = $item->getBasename() . PIECRUST_LINKER_DIR_SUFFIX;
-                    $this->linksCache[$key] = new Linker($this->pieCrust, $item->getPathname());
+                    $linker = new Linker($this->pieCrust, $item->getPathname(), null);
+                    $this->linksCache[$key] = $linker;
                 }
                 else
                 {
@@ -109,6 +127,7 @@ class Linker implements ArrayAccess, Iterator
                     $pageInfo = array(
                         'uri' => $uri,
                         'name' => $key,
+                        'is_self' => ($key == $this->selfKey),
                         'page' => new PageWrapper(Page::create($this->pieCrust, $uri, $item->getPathname()))
                     );
                     $this->linksCache[$key] = $pageInfo;
