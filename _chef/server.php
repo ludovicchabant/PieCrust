22de82a3c1dbd4f05fc2788eaa01971dbd0b78c3
Ludovic Chabant <ludovic@chabant.com>
2011-03-17 22:38:39 -0700

    Proper server command-line script with options and all.

--- _chef/server.php
+++ _chef/server.php
@@ -1,54 +1,77 @@
+#!/usr/bin/php
 <?php
-
-require 'ChefServer.class.php';
 
 error_reporting(E_ALL);
 
+require 'ChefServer.class.php';
+
+require_once 'Console/CommandLine.php';
+
+
+// Set up the command line parser.
+$parser = new Console_CommandLine(array(
+    'description' => 'Chef HTTP server, a web server that lets you easily preview and author your PieCrust website.',
+    'version' => PieCrust::VERSION
+));
+$parser->addOption('root', array(
+    'short_name'  => '-r',
+    'long_name'   => '--root',
+    'description' => "The directory in which we'll find '_content' and other such directories.",
+    'default'     => PIECRUST_ROOT_DIR,
+    'help_name'   => 'ROOT_DIR'
+));
+$parser->addOption('run_browser', array(
+    'short_name'  => '-n',
+    'long_name'   => '--nobrowser',
+    'description' => "Disables auto-running the default web browser when the server starts.",
+    'default'     => true,
+    'action'      => 'StoreFalse',
+    'help_name'   => 'RUN_BROWSER'
+));
+$parser->addOption('templates_dir', array(
+    'short_name'  => '-t',
+    'long_name'   => '--templates',
+    'description' => "An optional additional templates directory.",
+    'help_name'   => 'TEMPLATES_DIR'
+));
+$parser->addOption('pretty_urls', array(
+    'short_name'  => '-r',
+    'long_name'   => '--prettyurls',
+    'description' => "Enables pretty URLs for the baking (overrides the 'site/pretty_urls' configuration).",
+    'default'     => false,
+    'action'      => 'StoreTrue',
+    'help_name'   => 'PRETTY_URLS'
+));
+
+// Parse the command line.
 try
 {
-    $server = new ChefServer(dirname(dirname(__FILE__)) . '/website');
-    $server->run();
+    $result = $parser->parse();
+}
+catch (Exception $exc)
+{
+    $parser->displayError($exc->getMessage());
+    die();
+}
+
+// Validate arguments.
+$rootDir = $result->options['root'];
+if (!is_dir($rootDir))
+{
+    $parser->displayError("No such root directory: " . $rootDir, 1);
+    die();
+}
+
+try
+{
+    $server = new ChefServer($rootDir);
+    $server->run(array(
+                       'run_browser' => $result->options['run_browser'],
+                       'templates_dir' => $result->options['templates_dir']
+                       ));
 }
 catch (Exception $e)
 {
     echo $e;
     echo PHP_EOL;
 }
-
-
-/*Array
-(
-    [TZ] => Canada/Vancouver
-    [HTTP_HOST] => localhost:8888
-    [HTTP_CONNECTION] => keep-alive
-    [HTTP_USER_AGENT] => Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-US) AppleWebKit/534.16 (KHTML, like Gecko) Chrome/10.0.648.133 Safari/534.16
-    [HTTP_ACCEPT] => application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*\/*;q=0.5
-    [HTTP_ACCEPT_ENCODING] => gzip,deflate,sdch
-    [HTTP_ACCEPT_LANGUAGE] => en-US,en;q=0.8,fr-FR;q=0.6
-    [HTTP_ACCEPT_CHARSET] => ISO-8859-1,utf-8;q=0.7,*;q=0.3
-    [PATH] => /usr/bin:/bin:/usr/sbin:/sbin
-    [SERVER_SIGNATURE] => <address>Apache/2.0.63 (Unix) PHP/5.3.2 DAV/2 Server at localhost Port 8888</address> 
- 
-    [SERVER_SOFTWARE] => Apache/2.0.63 (Unix) PHP/5.3.2 DAV/2
-    [SERVER_NAME] => localhost
-    [SERVER_ADDR] => ::1
-    [SERVER_PORT] => 8888
-    [REMOTE_ADDR] => ::1
-    [DOCUMENT_ROOT] => /Applications/MAMP/htdocs
-    [SERVER_ADMIN] => you@example.com
-    [SCRIPT_FILENAME] => /Users/abdul/Work/WebSites/PieCrust/website/index.php
-    [REMOTE_PORT] => 55497
-    [GATEWAY_INTERFACE] => CGI/1.1
-    [SERVER_PROTOCOL] => HTTP/1.1
-    [REQUEST_METHOD] => GET
-    [QUERY_STRING] => 
-    [REQUEST_URI] => /piecrust/
-    [SCRIPT_NAME] => /piecrust/index.php
-    [PHP_SELF] => /piecrust/index.php
-    [REQUEST_TIME] => 1300154454
-    [argv] => Array
-        (
-        )
- 
-    [argc] => 0
-)*/
