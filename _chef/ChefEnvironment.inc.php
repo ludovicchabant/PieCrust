91c61796f8df754e644334bf2d93a617f90b8b29
ludovicchabant <none@none>
2011-03-18 15:54:51 -0700

    Fixed tabs.

--- _chef/ChefEnvironment.inc.php
+++ _chef/ChefEnvironment.inc.php
@@ -2,8 +2,8 @@
 
 // Add the PieCrust app directory to the include path.
 set_include_path(get_include_path() . PATH_SEPARATOR . 
-				 (dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '_piecrust') . PATH_SEPARATOR .
-				 (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'libs'));
+                 (dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '_piecrust') . PATH_SEPARATOR .
+                 (dirname(__FILE__) . DIRECTORY_SEPARATOR . 'libs'));
 
 if (!defined('PHP_VERSION_ID') or PHP_VERSION_ID < 50300)
 {
