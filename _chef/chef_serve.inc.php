5257208fbb063ed36b61969fdef0d3fb37a424dc
ludovicchabant <none@none>
2011-03-22 12:53:32 -0700

    Do not serve directory contents, so we can have _stuff directories with the same name as a page.

--- _chef/chef_serve.inc.php
+++ _chef/chef_serve.inc.php
@@ -16,6 +16,7 @@ function _chef_run_command($parser, $result)
     // Start serving!
     $server = new ChefServer($rootDir);
     $server->run(array(
+                       'list_directories' => false,
                        'run_browser' => $result->command->options['run_browser'],
                        'templates_dir' => $result->command->options['templates_dir']
                        ));
