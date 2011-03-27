fe2b17f6227ecfa72987d1dc50557061b5e3861e
Ludovic Chabant <ludovic@chabant.com>
2011-03-27 09:32:09 -0700

    Added SimpleFileProcessor unit tests. Added lib directories to test environment.

---
+++ tests/test-cases/SimpleFileProcessorTest.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once 'IFileProcessor.class.php';
+require_once 'SimpleFileProcessor.class.php';
+
+
+class SimpleFileProcessorTest extends PHPUnit_Framework_TestCase
+{
+    protected $rootDir;
+    
+    public function getRootDir()
+    {
+        if ($this->rootDir === null)
+        {
+            $this->rootDir = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'test-data' . DIRECTORY_SEPARATOR;
+        }
+        return $this->rootDir;
+    }
+    
+    public function processFileDataProvider()
+    {
+        return array(
+            array(
+                'less', 'css', 'path/to/some/file.less', 'file.css'
+            ),
+            array(
+                array('sass', 'scss'), 'css', '/path/to/some/file.sass', 'file.css'
+            ),
+            array(
+                array('sass', 'scss'), 'css', '/path/to/some/file.scss', 'file.css'
+            ),
+            array(
+                array('one', 'two'), array('1', '2'), '/path/to/some/file.one', 'file.1'
+            ),
+            array(
+                array('one', 'two'), array('1', '2'), '/path/to/some/file.two', 'file.2'
+            )
+        );
+    }
+    
+    /**
+	 * @dataProvider processFileDataProvider
+	 */
+	public function testProcessFile($inputExtensions, $outputExtensions, $inputPath, $expectedOutputPath)
+    {
+        $pc = new PieCrust(array('root' => $this->getRootDir(), 'debug' => true));
+        $sfp = new SimpleFileProcessor($inputExtensions, $outputExtensions);
+        $outputPath = $sfp->getOutputFilenames($inputPath);
+        $this->assertEquals($expectedOutputPath, $outputPath);
+    }
+}
--- tests/util.php
+++ tests/util.php
@@ -4,7 +4,8 @@
 error_reporting(E_ALL ^ E_NOTICE);
 
 set_include_path(get_include_path() . PATH_SEPARATOR . 
-				 (dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '_piecrust'));
+				 (dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '_piecrust') . PATH_SEPARATOR .
+				 (dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '_chef'));
 
 
 // Utility methods.
