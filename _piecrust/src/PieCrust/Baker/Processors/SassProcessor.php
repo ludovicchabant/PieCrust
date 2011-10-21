6a08be445659b806b27d4ff72e7c863621c6115a
Ludovic Chabant <ludovic@chabant.com>
2011-10-21 06:53:55 -0700

    Fixed a bug with the sass processor.

--- _piecrust/src/PieCrust/Baker/Processors/SassProcessor.php
+++ _piecrust/src/PieCrust/Baker/Processors/SassProcessor.php
@@ -16,7 +16,7 @@ class SassProcessor extends SimpleFileProcessor
     {
         $sassOptions = $this->pieCrust->getConfig('sass');
         if ($sassOptions == null) $sassOptions = array();
-        $sass = new SassParser($sassOptions);
+        $sass = new \SassParser($sassOptions);
         $css = $sass->toCss($inputPath);
         file_put_contents($outputPath, $css);
     }
