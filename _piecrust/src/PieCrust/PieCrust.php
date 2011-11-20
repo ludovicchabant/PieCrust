e2f5ced039c46d2d4ba5e9f6add75f26c2a173d6
Ludovic Chabant <ludovic@chabant.com>
2011-11-19 18:27:22 -0800

    Bumped version up.
    --HG--
    branch : stable

--- _piecrust/src/PieCrust/PieCrust.php
+++ _piecrust/src/PieCrust/PieCrust.php
@@ -23,7 +23,7 @@ class PieCrust
     /**
      * The current version of PieCrust.
      */
-    const VERSION = '0.1.3';
+    const VERSION = '0.1.4';
     
     /**
      * The application's source code directory.
