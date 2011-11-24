bdca8ef00f869c71d450460155bc4bbd9f93e310
Ludovic Chabant <ludovic@chabant.com>
2011-11-23 20:58:03 -0800

    Properly setup the Sass processor. Enable Compass extension by default.
    --HG--
    branch : stable

--- _piecrust/src/PieCrust/Baker/Processors/SassProcessor.php
+++ _piecrust/src/PieCrust/Baker/Processors/SassProcessor.php
@@ -4,19 +4,65 @@ namespace PieCrust\Baker\Processors;
 
 require_once 'PhamlP/sass/SassParser.php';
 
+use \SassParser;
+use PieCrust\PieCrust;
+
 
 class SassProcessor extends SimpleFileProcessor
 {
+    protected $sassOptions;
+
     public function __construct()
     {
         parent::__construct('sass', array('sass', 'scss'), 'css');
     }
     
+    public function initialize(PieCrust $pieCrust)
+    {
+        parent::initialize($pieCrust);
+
+        // User can specify the options for PhamlP's Sass parser
+        // through the 'sass' configuration section.
+        $sassOptions = $pieCrust->getConfigValue('sass');
+
+        // Let's add the default values.
+        if ($sassOptions == null)
+        {
+            $sassOptions = array();
+        }
+        $sassOptions = array_merge(
+            array(
+                'cache' => $pieCrust->isCachingEnabled(),
+                'cache_location' => $pieCrust->isCachingEnabled() ? $pieCrust->getCacheDir() . 'sass' : false, 
+                'css_location' => $pieCrust->getRootDir() . 'css',
+                'load_paths' => $pieCrust->getTemplatesDirs(),
+                'no_default_extensions' => false,
+                'extensions' => array()
+            ),
+            $sassOptions
+        );
+        if (!is_array($sassOptions['extensions']))
+        {
+            $sassOptions['extensions'] = array($sassOptions['extensions']);
+        }
+
+        // Add the default extensions (right now, just Compass).
+        if (!$sassOptions['no_default_extensions'])
+        {
+            if (!isset($sassOptions['extensions']['compass']))
+            {
+                $sassOptions['extensions']['compass'] = array(
+                    'project_path' => rtrim($pieCrust->getRootDir(), '/\\'),
+                    'http_path' => $pieCrust->getConfigValueUnchecked('site', 'root')
+                );
+            }
+        }
+        $this->sassOptions = $sassOptions;
+    }
+
     protected function doProcess($inputPath, $outputPath)
     {
-        $sassOptions = $this->pieCrust->getConfig('sass');
-        if ($sassOptions == null) $sassOptions = array();
-        $sass = new \SassParser($sassOptions);
+        $sass = new SassParser($this->sassOptions);
         $css = $sass->toCss($inputPath);
         file_put_contents($outputPath, $css);
     }
