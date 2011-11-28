8032157b0fb57fd7bf08dd029eb2e58604b3661a
Ludovic Chabant <ludovic@chabant.com>
2011-11-28 09:27:31 -0800

    Added more unit-tests for DirectoryBaker.

--- tests/src/test-cases/DirectoryBakerTest.php
+++ tests/src/test-cases/DirectoryBakerTest.php
@@ -127,4 +127,107 @@ class DirectoryBakerTest extends PHPUnit_Framework_TestCase
             vfsStream::inspect(new vfsStreamStructureVisitor())->getStructure()
         );
     }
+
+    public function testSkipPattern()
+    {
+        $structure = array(
+            'kitchen' => array(
+                'something.html' => 'This is a test page.',
+                '_hidden.html' => 'This is hidden',
+                'subdir' => array(
+                    '_important.html' => 'This should not be hidden.'
+                )
+            ),
+            '_counter' => array()
+        );
+        $root = vfsStream::create($structure);
+        
+        $pc = new MockPieCrust();
+        $pc->rootDir = vfsStream::url('root/kitchen');
+        $bakeDir = vfsStream::url('root/_counter');
+        $parameters = array(
+            'processors' => array('copy'),
+            'skip_patterns' => array('/^_/')
+        );
+        $baker = new DirectoryBaker($pc, $bakeDir, $parameters);
+        
+        $baker->bake();
+        $this->assertTrue(is_file(vfsStream::url('root/_counter/something.html')));
+        $this->assertTrue(is_file(vfsStream::url('root/_counter/subdir/_important.html')));
+        $this->assertFalse(is_file(vfsStream::url('root/_counter/_hidden.html')));
+    }
+
+    public function testUpToDate()
+    {
+        $structure = array(
+            'something.html' => 'This is a test page.',
+            '_counter' => array()
+        );
+        $root = vfsStream::create($structure);
+
+        $pc = new MockPieCrust();
+        $pc->rootDir = vfsStream::url('root');
+        $bakeDir = vfsStream::url('root/_counter');
+        $parameters = array(
+            'processors' => array('copy')
+        );
+        $baker = new DirectoryBaker($pc, $bakeDir, $parameters);
+        
+        $outFile = vfsStream::url('root/_counter/something.html');
+        $this->assertFalse(is_file($outFile));
+        usleep(1200000);
+        $baker->bake();
+        $this->assertTrue(is_file($outFile));
+        $this->assertEquals('This is a test page.', file_get_contents($outFile));
+        clearstatcache();
+        $mtime = filemtime($outFile);
+        $this->assertGreaterThan(filemtime(vfsStream::url('root/something.html')), $mtime);
+
+        usleep(1200000);
+        $baker->bake();
+        $this->assertTrue(is_file($outFile));
+        clearstatcache();
+        $this->assertEquals($mtime, filemtime($outFile));
+        $this->assertEquals('This is a test page.', file_get_contents($outFile));
+
+        usleep(1200000);
+        file_put_contents(vfsStream::url('root/something.html'), 'New content!');
+        $baker->bake();
+        $this->assertTrue(is_file($outFile));
+        clearstatcache();
+        $this->assertGreaterThan($mtime, filemtime($outFile));
+        $this->assertEquals('New content!', file_get_contents($outFile));
+    }
+    
+    public function testForceBake()
+    {
+        $structure = array(
+            'forced.html' => 'This is a test page.',
+            '_counter' => array()
+        );
+        $root = vfsStream::create($structure);
+
+        $pc = new MockPieCrust();
+        $pc->rootDir = vfsStream::url('root');
+        $bakeDir = vfsStream::url('root/_counter');
+        $parameters = array(
+            'processors' => array('copy'),
+            'force_patterns' => array('/forced/')
+        );
+        $baker = new DirectoryBaker($pc, $bakeDir, $parameters);
+        
+        $outFile = vfsStream::url('root/_counter/forced.html');
+        $this->assertFalse(is_file($outFile));
+        $baker->bake();
+        $this->assertTrue(is_file($outFile));
+        $this->assertEquals('This is a test page.', file_get_contents($outFile));
+        $mtime = filemtime($outFile);
+
+        usleep(1000000);
+        $baker->bake();
+        $this->assertTrue(is_file($outFile));
+        clearstatcache();
+        $this->assertGreaterThan($mtime, filemtime($outFile));
+        $this->assertEquals('This is a test page.', file_get_contents($outFile));
+    }
 }
