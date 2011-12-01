58b05320e4343f506446f80fdb6fe85ced4e2b96
Ludovic Chabant <ludovic@chabant.com>
2011-12-01 10:46:44 -0800

    Updated version to 0.2.0.
    --HG--
    branch : stable

--- _piecrust/src/PieCrust/PieCrustDefaults.php
+++ _piecrust/src/PieCrust/PieCrustDefaults.php
@@ -11,7 +11,7 @@ class PieCrustDefaults
     /**
      * The current version of PieCrust.
      */
-    const VERSION = '0.2.0-dev';
+    const VERSION = '0.2.0';
     
     /**
      * The application's source code directory.
