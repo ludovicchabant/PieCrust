f49526a082ed79e8cf793c78b8e453988acf8ebd
Ludovic Chabant <ludovic@chabant.com>
2011-12-05 11:02:27 -0800

    Fixed a bug in the `chef init` command when the root directory of the new website doesn't exist. Took out the IIS and Apache files from the created website, and added `--apache` and `--iis` options to add them back. Clarified ending messages.
    --HG--
    branch : stable

--- _piecrust/src/PieCrust/Chef/Commands/InitCommand.php
+++ _piecrust/src/PieCrust/Chef/Commands/InitCommand.php
@@ -26,27 +26,46 @@ class InitCommand implements IChefCommand
     public function setupParser(Console_CommandLine $initParser)
     {
         $initParser->description = "Creates a new empty PieCrust website.";
+        $initParser->addOption('apache', array(
+            'long_name'   => '--apache',
+            'description' => "Adds the files needed to run or preview the website using Apache.",
+            'default'     => false,
+            'action'      => 'StoreTrue'
+        ));
+        $initParser->addOption('iis', array(
+            'long_name'   => '--iis',
+            'description' => "Adds the files needed to run or preview the website using IIS.",
+            'default'     => false,
+            'action'      => 'StoreTrue'
+        ));
     }
     
     public function run(Console_CommandLine $parser, Console_CommandLine_Result $result)
     {
         $rootDir = $result->command->args['root'];
-        $this->initializeWebsite($rootDir);
+        $apache = $result->command->options['apache'];
+        $iis = $result->command->options['iis'];
+        $this->initializeWebsite($rootDir, array('apache' => $apache, 'iis' => $iis));
     }
 
-    public function initializeWebsite($rootDir)
+    public function initializeWebsite($rootDir, $options = array())
     {
+        // Validate the options.
+        $options = array_merge(
+            array(
+                'apache' => false,
+                'iis' => false
+            ),
+            $options
+        );
+
+        // Make sure the root directory exists.
         $rootDir = rtrim($rootDir, '/\\') . DIRECTORY_SEPARATOR;
         if (!is_dir($rootDir))
         {
             $this->createDirectory($rootDir, '');
-            throw new Exception("The given root is not a directory: ".$rootDir);
         }
-        if (!is_writable($rootDir))
-        {
-            throw new Exception("The given root directory is not writeable: ".$rootDir);
-        }
-        
+
         // Create the directory structure.
         $this->createDirectory($rootDir, PieCrustDefaults::CACHE_DIR, true);
         $this->createDirectory($rootDir, PieCrustDefaults::CONTENT_DIR);
@@ -55,9 +74,6 @@ class InitCommand implements IChefCommand
         $this->createDirectory($rootDir, PieCrustDefaults::CONTENT_TEMPLATES_DIR);
         
         // Create the basic files.
-        $this->createSystemFile('htaccess', $rootDir, '.htaccess');
-        $this->createSystemFile('web.config', $rootDir, 'web.config');
-        $this->createBootstraper($rootDir);
         $this->createYamlFile($rootDir, PieCrustDefaults::CONFIG_PATH, array(
             'site' => array(
                 'title' => 'My New Website',
@@ -71,16 +87,38 @@ class InitCommand implements IChefCommand
         ));
         $this->createSystemFile('default_template.html', $rootDir, PieCrustDefaults::CONTENT_TEMPLATES_DIR . PieCrustDefaults::DEFAULT_PAGE_TEMPLATE_NAME . '.html');
         $this->createSystemFile('default_index.html', $rootDir, PieCrustDefaults::CONTENT_PAGES_DIR . PieCrustDefaults::INDEX_PAGE_NAME . '.html');
+
+        if ($options['apache'])
+        {
+            $this->createSystemFile('htaccess', $rootDir, '.htaccess');
+        }
+        if ($options['iis'])
+        {
+            $this->createSystemFile('web.config', $rootDir, 'web.config');
+        }
+        if ($options['apache'] || $options['iis'])
+        {
+            $this->createBootstraper($rootDir);
+        }
         
         echo PHP_EOL;
         echo "PieCrust website created in: " . $rootDir . PHP_EOL;
         echo PHP_EOL;
-        echo "Please edit '".$rootDir."index.php' to fix the relative path to '_piecrust/piecrust.php'.";
-        echo PHP_EOL;
-        echo "If your webserver runs under a different user, you may have to change the root directory's permissions ".
-             "so that it's readable." . PHP_EOL;
-        echo "You may have to also change the permissions on the '_cache' directory." . PHP_EOL;
-        echo PHP_EOL;
+
+        if ($options['apache'] || $options['iis'])
+        {
+            echo "Please edit '".$rootDir."index.php' to fix the relative path to '_piecrust/piecrust.php'.";
+            echo PHP_EOL;
+            echo "If your webserver runs under a different user, you may have to change the root directory's permissions so that it's readable." . PHP_EOL;
+            echo "You may have to also change the permissions on the '_cache' directory so that it's readable." . PHP_EOL;
+            echo PHP_EOL;
+        }
+        else
+        {
+            echo "Run 'chef serve' on this directory to preview it." . PHP_EOL;
+            echo "Run 'chef bake' on this directory to generate the static files." . PHP_EOL;
+            echo PHP_EOL;
+        }
     }
     
     protected function createDirectory($rootDir, $dir, $makeWritable = false)
@@ -115,7 +153,7 @@ EOD;
     {
         echo "Generating " . $destination . PHP_EOL;
         $yaml = new \sfYamlDumper();
-        $contents = $yaml->dump($data);
+        $contents = $yaml->dump($data, 3);
         file_put_contents($rootDir . $destination, $contents);
     }
 }
