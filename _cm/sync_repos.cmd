ade0a93ec57843b5973dded2fe59327e9d5d9dbe
Ludovic Chabant <ludovic@chabant.com>
2012-01-22 22:13:46 -0800

    No more 'sample' repository.

--- _cm/sync_repos.cmd
+++ _cm/sync_repos.cmd
@@ -10,6 +10,3 @@ set SAMPLE_REPO=%THIS_REPO%\..\PieCrust_Sample
 rem Exporting app-only repository.
 hg convert --filemap %CWD%piecrust_app_filemap %THIS_REPO% %APP_REPO%
 
-rem Exporting sample website repository.
-hg convert --filemap %CWD%piecrust_sample_filemap %THIS_REPO% %SAMPLE_REPO%
-
