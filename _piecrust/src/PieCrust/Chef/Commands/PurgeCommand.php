48e7530408b4cb228816f0a708d7eb019e2fb151
Ludovic Chabant <ludovic@chabant.com>
2012-02-04 20:31:44 -0800

    Added 'purge' chef command.

---
+++ _piecrust/src/PieCrust/Chef/Commands/PurgeCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace PieCrust\Chef\Commands;
+
+use \Console_CommandLine;
+use \Console_CommandLine_Result;
+use PieCrust\IPieCrust;
+use PieCrust\PieCrustException;
+use PieCrust\Chef\ChefContext;
+use PieCrust\IO\FileSystem;
+
+
+class PurgeCommand extends ChefCommand
+{
+    public function getName()
+    {
+        return 'purge';
+    }
+    
+    public function setupParser(Console_CommandLine $rootParser)
+    {
+        $rootParser->description = "Purges the website's cache directory.";
+    }
+
+    public function run(ChefContext $context)
+    {
+        $cacheDir = $context->getApp()->getCacheDir();
+        if (!$cacheDir)
+            throw new PieCrustException("The website seems to have caching disabled.");
+        if (!is_dir($cacheDir))
+            throw new PieCrustException("The cache directory doesn't exist: {$cacheDir}");
+
+        $context->getLog()->info("Purging cache: {$cacheDir}");
+        FileSystem::deleteDirectoryContents($cacheDir);
+    }
+}
+
+
