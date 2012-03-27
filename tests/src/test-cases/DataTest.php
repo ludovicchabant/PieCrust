7c9761f25f9a576185d8eb0c216bc43582d556f8
Ludovic Chabant <ludovic@chabant.com>
2012-03-26 23:22:40 -0700

    Added some template data tests.

---
+++ tests/src/test-cases/DataTest.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once 'unittest_setup.php';
+
+use PieCrust\PieCrust;
+use PieCrust\Page\Page;
+use PieCrust\Data\PaginationData;
+
+
+class DataTest extends PHPUnit_Framework_TestCase
+{
+    public function testPaginationData()
+    {
+        $fs = MockFileSystem::create()
+            ->withPage(
+                'blah',
+                array('title' => "My Blah", 'foo' => "My Foo", 'format' => 'none'),
+                'Some contents.'
+            );
+
+        $app = new PieCrust(array('root' => vfsStream::url('root/kitchen')));
+        $page = Page::createFromUri($app, '/blah');
+
+        $data = new PaginationData($page);
+        $this->assertEquals('My Blah', $data['title']);
+        $this->assertEquals('My Foo', $data['foo']);
+        $this->assertEquals('/?/blah', $data['url']);
+        $this->assertEquals('blah', $data['slug']);
+        $this->assertEquals('Some contents.', $data['content']);
+        $this->assertFalse($data['has_more']);
+    }
+}
+
