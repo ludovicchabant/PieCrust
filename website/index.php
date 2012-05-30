37098bd60169dedf32416b30a86bd56fa280d6eb
Ludovic Chabant <ludovic@chabant.com>
2012-05-29 17:45:13 -0700

    Fixed sample website `index.php`.

--- website/index.php
+++ website/index.php
@@ -1,4 +1,7 @@
 <?php
-require '../_piecrust/version_check.php';
-require '../_piecrust/piecrust.php';
+if (PHP_VERSION_ID < 50300)
+{
+    trigger_error("PieCrust requires PHP 5.3+", E_USER_ERROR);
+}
+require '../piecrust.php';
 piecrust_run();
