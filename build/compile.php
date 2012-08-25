46c1e0770113896e6e0b261b49f23120ba15db39
Ludovic Chabant <ludovic@chabant.com>
2012-08-24 18:45:40 -0700

    PHAR compiler script enhancements: - Checks that the `php.ini` has the correct settings. - Can take the name of the PHAR file as an argument.
    --HG--
    branch : stable

--- build/compile.php
+++ build/compile.php
@@ -4,6 +4,13 @@ require __DIR__ . '/../piecrust.php';
 
 piecrust_setup('compiler');
 
+if (!Phar::canWrite())
+    die("Can't create Phar file because 'phar.readonly' is enabled in 'php.ini'." . PHP_EOL);
+
+$pharFile = 'piecrust.phar';
+if ($argc > 1)
+    $pharFile = $argv[1];
+
 $compiler = new PieCrust\Compiler();
-$compiler->compile();
+$compiler->compile($pharFile);
 
