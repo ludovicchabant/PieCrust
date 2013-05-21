ef3126de56869afecb7ed5671ea598502b16a62d
Ludovic Chabant <ludovic@chabant.com>
2013-05-20 22:26:35 -0700

    Removed deprecated chef scripts.

--- _piecrust/piecrust.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Deprecated file, use the one in the root dir.
-trigger_error(__FILE__ . ' is deprecated, please use /piecrust.php instead.' . PHP_EOL, E_USER_DEPRECATED);
-include dirname(__DIR__) . '/piecrust.php';
