33d01d70d7a989d51511fa4418d169df9180afe6
Ludovic Chabant <ludovic@chabant.com>
2013-06-13 09:20:28 -0700

    Updated CHANGELOG, bumped version to 1.0.1.
    --HG--
    branch : stable

--- src/PieCrust/PieCrustDefaults.php
+++ src/PieCrust/PieCrustDefaults.php
@@ -11,7 +11,7 @@ class PieCrustDefaults
     /**
      * The current version of PieCrust.
      */
-    const VERSION = '1.0.0';
+    const VERSION = '1.0.1';
 
     /**
      * The current version of the configuration.
